30937ffb2c8c71843225ad2f0a8ff8f7444eb7f2
Fernando Cardenas <fernando.cardenasm@gmail.com>
2021-04-29 21:22:14 +0200

    AppCoordinator does not push Views upon creation

--- MarvelUniverseForAll/AppCoordinator.swift
+++ MarvelUniverseForAll/AppCoordinator.swift
@@ -8,54 +8,16 @@
 import Combine
 import SwiftUI
 
-class UserManagementCoordinator {
+public class AppCoordinator {
     
-    var cancellables = Set<AnyCancellable>()
-    let finishedSubject = PassthroughSubject<Void, Never>()
-    
-    private let authService: AuthService
-    private var navController: UINavigationController?
-    
-    init(authService: AuthService) {
-        self.authService = authService
-    }
-    
-    func start(navController: UINavigationController) {
-        self.navController = navController
-        
-        navController.viewControllers.insert(signInViewController(), at: 0)
-        navController.popToRootViewController(animated: true)
-        
-        DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
-            self.authService.signIn().sink { [weak self] _ in
-                print("User logged In")
-                self?.finishedSubject.send(completion: .finished)
-            }.store(in: &self.cancellables)
-        }
-    }
-    
-    func finished() -> AnyPublisher<Void, Never> {
-        finishedSubject.eraseToAnyPublisher()
-    }
-    
-    func signInViewController() -> UIHostingController<SignInView> {
-        let viewModel = SignInViewModel()
-        let view = SignInView(viewModel: viewModel)
-        
-        return UIHostingController(rootView: view)
-    }
-}
-
-class AppCoordinator {
-    
-    lazy var navController = UINavigationController()
-    lazy var rootViewController: UIViewController = navController
+    public private(set) lazy var navController = UINavigationController()
+    public private(set) lazy var rootViewController: UIViewController = navController
     
     var cancellables = Set<AnyCancellable>()
     
-    let userManagementCoordinator: UserManagementCoordinator
+    private let userManagementCoordinator: UserManagementCoordinator
     
-    init(userManagementCoordinator: UserManagementCoordinator) {
+    public init(userManagementCoordinator: UserManagementCoordinator) {
         self.userManagementCoordinator = userManagementCoordinator
     }
     
--- MarvelUniverseForAllTests/AppCoordinatorTests.swift
+++ MarvelUniverseForAllTests/AppCoordinatorTests.swift
@@ -5,8 +5,26 @@
 //  Created by Fernando Cardenas on 29.04.21.
 //
 
+import Combine
+import MarvelUniverseForAll
 import XCTest
 
 class AppCoordinatorTests: XCTestCase {
     
+    func test_init_doesNotPushViews() {
+        let userManagementCoordinator = UserManagementCoordinatorSpy()
+        let sut = AppCoordinator(userManagementCoordinator: userManagementCoordinator)
+        
+        XCTAssertEqual(sut.navController, sut.rootViewController)
+    }
+}
+
+class UserManagementCoordinatorSpy: UserManagementCoordinator {
+    
+    func start(navController: UINavigationController) {
+    }
+    
+    func finished() -> AnyPublisher<Void, Never> {
+        Just(()).eraseToAnyPublisher()
+    }
 }
