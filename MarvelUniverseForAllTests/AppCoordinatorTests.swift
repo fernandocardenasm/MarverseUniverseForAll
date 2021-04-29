8da480ccd4f84d30aad7f1d06b889d0342c78497
Fernando Cardenas <fernando.cardenasm@gmail.com>
2021-04-29 20:53:27 +0200

    Add file for AppCoordinatorTests

---
+++ MarvelUniverseForAllTests/AppCoordinatorTests.swift
@@ -0,0 +1,12 @@
+//
+//  AppCoordinatorTests.swift
+//  MarvelUniverseForAllTests
+//
+//  Created by Fernando Cardenas on 29.04.21.
+//
+
+import XCTest
+
+class AppCoordinatorTests: XCTestCase {
+    
+}
