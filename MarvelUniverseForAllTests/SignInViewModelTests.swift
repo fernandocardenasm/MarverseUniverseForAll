94f644313fdfef0eb397f2bf8ae14d997ea5c747
Fernando Cardenas <fernando.cardenasm@gmail.com>
2021-05-04 08:17:22 +0200

    init for SignInViewModel does not start signing In

---
+++ MarvelUniverseForAllTests/SignInViewModelTests.swift
@@ -0,0 +1,33 @@
+//
+//  SignInViewModelTests.swift
+//  MarvelUniverseForAllTests
+//
+//  Created by Fernando Cardenas on 04.05.21.
+//
+
+import Combine
+import MarvelUniverseForAll
+import XCTest
+
+class SignInViewModel: ObservableObject {
+    
+    @Published var isSigningIn: Bool = false
+    
+    let signInSubject = PassthroughSubject<Void, Never>()
+    let skipSignInSubject = PassthroughSubject<Void, Never>()
+    
+    func signIn() {
+        isSigningIn = true
+    }
+}
+
+
+class SignInViewModelTests: XCTestCase {
+    
+    var cancellables = Set<AnyCancellable>()
+    
+    func test_init_doesNotStartSigningIn() {
+        let sut = SignInViewModel()
+        XCTAssertFalse(sut.isSigningIn)
+    }
+}
