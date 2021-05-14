8e402c3b37e8416e33f6e1b14a8bf6b04847351a
Fernando Cardenas <fernando.cardenasm@gmail.com>
2021-05-14 14:55:33 +0200

    method viewDidLoad setsViewControllers for TabBarController

--- MarvelUniverseForAllTests/TabBar/TabBarControllerTests.swift
+++ MarvelUniverseForAllTests/TabBar/TabBarControllerTests.swift
@@ -30,6 +30,15 @@ class TabBarControllerTests: XCTestCase {
         expect(barItem: sut.settingsNavController.tabBarItem, toBeEqualTo: settingsBarItem)
     }
     
+    func test_viewDidLoad_setsViewControllers() {
+        let sut = TabBarController()
+        sut.loadViewIfNeeded()
+        
+        XCTAssertEqual(sut.viewControllers?.count, 2)
+        XCTAssertEqual(sut.viewControllers?[0], sut.homeNavController)
+        XCTAssertEqual(sut.viewControllers?[1], sut.settingsNavController)
+    }
+    
     func expect(barItem: UITabBarItem, toBeEqualTo comparedBarItem: UITabBarItem) {
         XCTAssertEqual(barItem.title, comparedBarItem.title)
         XCTAssertEqual(barItem.image, comparedBarItem.image)
